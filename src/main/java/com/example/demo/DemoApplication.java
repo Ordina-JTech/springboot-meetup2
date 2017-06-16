c06e32551a2d14436d1563c4dbac66c104758229
Johan Vogelzang <johan.vogelzang@gmail.com>
2017-06-16 14:45:24 +0200

    step 2 : Added JPA entity and repository. Tested it with @DataJPATest.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,17 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppConfig.class)
