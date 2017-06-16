19fbd9ec4dc13b06f9231ae10e9d399b65737ee9
Johan Vogelzang <johan.vogelzang@gmail.com>
2017-06-16 14:02:08 +0200

    step 1 : @EnableConfigurationProperties on application level

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 class GreeterController {
+
+    private AppConfig appConfig;
 
     @GetMapping
     public String greet(){
-        return "Hallo allemaal!";
+        return appConfig.getGreeting();
     }
 }
